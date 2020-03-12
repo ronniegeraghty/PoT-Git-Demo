4c595cb81a078a59a78807e6e703b5698d017c36
Ronnie Geraghty <ronniegerag@gmail.com>
2020-03-12 11:56:05 -0400

    New Ronnie conflicting change

--- HCAZ_Source/cobol/HCAPDB01.cbl
+++ HCAZ_Source/cobol/HCAPDB01.cbl
@@ -16,7 +16,7 @@
       *   To add patient's name, address and date of birth to the      *
       *  DB2 patient table creating a new patient entry.               *
       *                                                                *
-      *    Change by Ronnie
+      *    Change by Ronnie, New Ronnie conflicting change
       *    changed by Regi
       ******************************************************************
        IDENTIFICATION DIVISION.
